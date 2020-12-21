9796b6bde6c7eab2ce95af04a27d3eb723f11b39
wangrl2016 <wangrl2016@gmail.com>
2020-12-21 20:20:47 +0800

    完成简单的PhotoEditor编写

--- modules/photoeditor/src/PhotoEditor.cpp
+++ modules/photoeditor/src/PhotoEditor.cpp
@@ -5,6 +5,8 @@
 #include "include/core/SkGraphics.h"
 #include "include/core/SkCanvas.h"
 #include "include/core/SkSurface.h"
+#include "include/core/SkFont.h"
+#include "include/effects/SkGradientShader.h"
 #include "modules/photoeditor/include/PhotoEditor.h"
 
 sk_app::Application* sk_app::Application::Create(int argc, char** argv, void* platformData) {
@@ -42,10 +44,50 @@ void PhotoEditor::onPaint(SkSurface* surface) {
     // Draw a rectangle with red paint.
     SkRect rect = SkRect::MakeXYWH(10, 10, 128, 128);
     canvas->drawRect(rect, paint);
+
+    // Set up a linear gradient and draw a circle.
+    {
+        SkPoint linearPoints[] = {{0,   0},
+                                  {300, 300}};
+        SkColor linearColors[] = {
+                SK_ColorGREEN, SK_ColorBLACK};
+
+        paint.setShader(SkGradientShader::MakeLinear(linearPoints, linearColors, nullptr, 2,
+                                                     SkTileMode::kMirror));
+        paint.setAntiAlias(true);
+
+        canvas->drawCircle(200, 200, 64, paint);
+
+        // Detach shader.
+        paint.setShader(nullptr);
+    }
+
+    // Draw a message with a nice black paint.
+    SkFont font;
+    font.setSubpixel(true);
+    font.setSize(20);
+    paint.setColor(SK_ColorBLACK);
+
+    canvas->save();
+    static const char message[] = "Hello World";
+
+    // Translate and rotate.
+    canvas->translate(300, 300);
+    mRotationAngle += 0.2f;
+    if (mRotationAngle > 360) {
+        mRotationAngle -= 360;
+    }
+    canvas->rotate(mRotationAngle);
+
+    // Draw the text.
+    canvas->drawSimpleText(message, strlen(message), SkTextEncoding::kUTF8, 0, 0, font, paint);
+
+    canvas->restore();
 }
 
 void PhotoEditor::onIdle() {
-
+    // Just re-paint continuously.
+    mWindow->inval();
 }
 
 void PhotoEditor::onBackendCreated() {
